45ec942a1774c3691020b8d717ef9186a976434a
muhammadsulaymannkhan <muhammadsulaymannkhan@gmail.com>
2022-10-01 17:40:14 +0500

    first page completed

--- src/components/common/GenericLanding.jsx
+++ src/components/common/GenericLanding.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import landing from "../../assets/landing.svg"
 
 export default function GenericLanding() {
   return (
-    <>
-      <h2>Hello</h2>
-    </>
+    <div className="landing-container">
+      <div className="landing-heading">Imagine if <span className="landing-heading-focus">Snapchat</span><br/> had events</div>
+      <p className="landing-text"> Easily host and share events with your friends across any social media.</p>
+      <div className="landing-image-container">
+      <img src={landing} className="landing-image"/>
+      </div>
+      <div className="landing-button">Create my event</div>
+    </div>
   );
 }
